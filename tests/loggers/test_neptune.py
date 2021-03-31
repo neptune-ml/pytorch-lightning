edc56c6e631c85e8204243e3c87166677e766ab6
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-03-31 23:44:40 +0200

    Initial `NeptuneLogger` with tests

---
+++ tests/loggers/test_neptune.py
@@ -0,0 +1,87 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from unittest.mock import MagicMock, patch
+from unittest import skip
+
+import torch
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import NeptuneLogger
+from tests.helpers import BoringModel
+
+
+@patch('pytorch_lightning.loggers.neptune.neptune_alpha')
+def test_neptune_online(neptune_alpha):
+    logger = NeptuneLogger(api_key='test', project_name='project')
+
+    created_run = neptune_alpha.init()
+
+    # It's important to check if the internal variable _experiment was initialized in __init__.
+    # Calling logger.experiment would cause a side-effect of initializing _experiment,
+    # if it wasn't already initialized.
+    assert logger._run_instance is None
+    _ = logger.experiment
+    assert logger._run_instance == created_run
+    assert logger.name == created_run.name
+    assert logger.version == str(created_run._uuid)
+
+
+@skip(reason='Not implemented yet')
+@patch('pytorch_lightning.loggers.neptune.neptune_alpha')
+def test_neptune_existing_experiment(neptune_alpha):
+    logger = NeptuneLogger(run='TEST-123')
+    # neptune.Session.with_default_backend().get_project().get_experiments.assert_not_called()
+    run = logger.experiment
+    # neptune.Session.with_default_backend().get_project().get_experiments.assert_called_once_with(id='TEST-123')
+    assert logger.experiment_name == run.get_system_properties()['name']
+    assert logger.params == run.get_parameters()
+    assert logger.properties == run.get_properties()
+    assert logger.tags == run.get_tags()
+
+
+@patch('pytorch_lightning.loggers.neptune.neptune_alpha')
+def test_neptune_additional_methods(neptune_alpha):
+    logger = NeptuneLogger(api_key='test', project_name='project')
+
+    run_mock = MagicMock()
+    neptune_alpha.init.return_value = run_mock
+
+    logger.experiment['key1'].log(torch.ones(1))
+    run_mock.__getitem__.assert_called_once_with('key1')
+    run_mock.__getitem__().log.assert_called_once_with(torch.ones(1))
+    run_mock.reset_mock()
+
+@patch('pytorch_lightning.loggers.neptune.neptune_alpha')
+def test_neptune_leave_open_experiment_after_fit(neptune_alpha, tmpdir):
+    """Verify that neptune experiment was closed after training"""
+    model = BoringModel()
+
+    def _run_training(logger):
+        logger._run_instance = MagicMock()
+        trainer = Trainer(
+            default_root_dir=tmpdir,
+            max_epochs=1,
+            limit_train_batches=0.05,
+            logger=logger,
+        )
+        assert trainer.log_dir is None
+        trainer.fit(model)
+        assert trainer.log_dir is None
+        return logger
+
+    logger_close_after_fit = _run_training(NeptuneLogger(offline_mode=True))
+    assert logger_close_after_fit.experiment.stop.call_count == 1
+
+    logger_open_after_fit = _run_training(NeptuneLogger(offline_mode=True, close_after_fit=False))
+    assert logger_open_after_fit.experiment.stop.call_count == 0
